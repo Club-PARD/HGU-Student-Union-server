70a08e5670dbcab5b15fe0cd90d95ac83a1d3a09
lukehongg <lukehongg@handong.ac.kr>
2023-09-02 20:04:56 +0900

    allowedMethod

--- src/main/java/com/server/HGUStudentUnion_server/common/WebConfig.java
+++ src/main/java/com/server/HGUStudentUnion_server/common/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig extends WebMvcConfigurationSupport {
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:3000","http://localhost:8080") // 허용할 출처
                 .allowedOrigins("*")
-                .allowedMethods("*"); // 허용할 HTTP method
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS"); // 허용할 HTTP method
 //                .allowCredentials(true) // 쿠키 인증 요청 허용
 //                .maxAge(3000);// 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
